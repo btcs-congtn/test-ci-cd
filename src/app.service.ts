e34488dae192eae36159a59d54a51f6e7a272faa
TranNgocCong <congtn121999@gmail.com>
2022-01-28 20:53:46 +0700

    ok

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): { status: string; data: string } {
-    return { status: 'available', data: `chao ban nhe ${process.env.DB}` };
+    return { status: 'available', data: `chao ban nhe ${process.env.DB} 3` };
   }
 
   getHealth(): { status: string } {
